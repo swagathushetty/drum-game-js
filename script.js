6cd41f62a0cf53b909c88dba014d171e902633c9
swagath shetty <46868295+swagathushetty@users.noreply.github.com>
2020-04-08 17:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function playSound(e){
+    console.log(e) //keyBoard event
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!audio) return
+
+    audio.currentTime = 0 //rewind to start
+    audio.play()
+
+    key.classList.add('playing')
+}
+
+
+function removeTranition(e){
+    if(e.propertyName!=='transform') return
+    console.log(e.propertyName)
+    this.classList.remove('playing')
+}
+
+const keys=document.querySelectorAll('.key');
+keys.forEach(key=>{
+    key.addEventListener('transitionend',removeTranition)
+})
+
+
+window.addEventListener('keydown', playSound)
